3d0af8fd328ce156bbbcb46e95ef1540df52e299
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-08 13:17:57 +0530

    files added

---
+++ Attendance computation/Attendance computation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
+
