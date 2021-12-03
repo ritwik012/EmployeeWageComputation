b0c6f138de7da630a98550420d4b80c939eba743
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 20:19:38 +0530

    [Rutwik]: Added Attendance Computation  File

---
+++ EmployeeWageComputation/EmployeeWageProblem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeWageProblem
+    {
+        public const int IS_FULL_TIME = 1;
+        public void EmployeeAttendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Employee Wage Computation Problem");
+            EmployeeWageProblem employee = new EmployeeWageProblem();
+            employee.EmployeeAttendance();
+        }
+    }
+}
