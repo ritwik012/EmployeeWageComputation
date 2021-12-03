952a916e01d3f306c39e70d81945b487e445ec1d
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 20:26:58 +0530

    [Rutwik]: Added Switch Case  File

--- EmployeeWageComputation/EmployeeWageProblem.cs
+++ EmployeeWageComputation/EmployeeWageProblem.cs
@@ -12,17 +12,17 @@ namespace EmployeeWageProblem
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0; ;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage : " + empWage);
