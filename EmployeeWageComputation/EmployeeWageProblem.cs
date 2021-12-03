de7bf3d016c571a9e4abd9b90441f8b4c36856ba
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 20:24:55 +0530

    [Rutwik]: Added Working Employee Wage  File

--- EmployeeWageComputation/EmployeeWageProblem.cs
+++ EmployeeWageComputation/EmployeeWageProblem.cs
@@ -6,13 +6,17 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWageProblem
     {
-        public const int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
         int empHrs = 0, empWage = 0;
         public void DailyEmployeeWage()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
