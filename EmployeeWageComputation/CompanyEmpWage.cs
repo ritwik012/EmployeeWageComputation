4c73e287c46eb4270085a0a83c4d2cf36e80fb65
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 20:45:17 +0530

    [Rutwik]: Added Manage Employee Wage File

---
+++ EmployeeWageComputation/CompanyEmpWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour, numOfWorkingDays, maxHoursPerMonth, totalEmpWage;
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            return "The Total Employee Wage for Company " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,12 +7,10 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Computation Problem");
-            EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 20, 100);
-            EmpWageBuilder pvp = new EmpWageBuilder("PVP", 25, 24, 160);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            pvp.computeEmpWage();
-            Console.WriteLine(pvp.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 100);
+            empWageBuilder.addCompanyEmpWage("PVP", 30, 24, 160);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
